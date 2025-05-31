b25124605ff94cce504417addde4a6bd617ed5be
RohitP2005 <rohitthevar@gmail.com>
2025-05-31 23:17:30 +0530

    chatbot added

--- backend/app.py
+++ backend/app.py
@@ -3,12 +3,14 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 load_dotenv()
 from routes.resume_routes import resume_blueprint
+from routes.chatbot_routes import chat_blueprint
 
 app = Flask(__name__)
 CORS(app)
 
 # Register routes
 app.register_blueprint(resume_blueprint)
+app.register_blueprint(chat_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
